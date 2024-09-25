5bffb7ff2ebe10e2f53fe1f84ccd6ca7a9cf65bf
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-09-24 23:35:16 -0500

    Implemented the addDefaultEvents() method

--- src/edu/uca/dhoelzeman/gui/EventPlanner.java
+++ src/edu/uca/dhoelzeman/gui/EventPlanner.java
@@ -1,24 +1,35 @@
 package edu.uca.dhoelzeman.gui;
 
+import edu.uca.dhoelzeman.console.Deadline;
+import edu.uca.dhoelzeman.console.Meeting;
+
 import javax.swing.*;
 import java.awt.*;
+import java.time.LocalDateTime;
 
 public class EventPlanner {
     public static void main(String[] args) {
         var frame = new JFrame("Event Planner");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(1000, 600));
+        frame.setLocation(200, 200);
 //        frame.setResizable(false);
         frame.setIconImage(new ImageIcon("src/images/calendar.png").getImage());
 
-        frame.add(new EventListPanel());
+        var eventListPanel = new EventListPanel();
+        addDefaultEvents(eventListPanel);
+
+        frame.add(eventListPanel);
 
 
         frame.pack();
         frame.setVisible(true);
     }
 
-    static void addDefaultEvents(EventPanel events) {
+    static void addDefaultEvents(EventListPanel eventListPanel) {
+        eventListPanel.events.add(new Meeting("B", LocalDateTime.of(2003, 3, 7, 13, 30), LocalDateTime.of(2003, 3, 7, 12, 35), "Default Location"));
+        eventListPanel.events.add(new Deadline("A", LocalDateTime.of(2003, 3, 7, 12, 30)));
 
+        eventListPanel.addOrUpdateEvents();
     }
 }
