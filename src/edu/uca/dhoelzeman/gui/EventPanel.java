a4647be6354ac9868750e6b1b01f1d5ab810fc54
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-09-24 23:35:03 -0500

    Utilized the getDisplayStrings() method to display to the user

--- src/edu/uca/dhoelzeman/gui/EventPanel.java
+++ src/edu/uca/dhoelzeman/gui/EventPanel.java
@@ -1,29 +1,37 @@
 package edu.uca.dhoelzeman.gui;
 
 import edu.uca.dhoelzeman.console.Completable;
-import edu.uca.dhoelzeman.console.Deadline;
 import edu.uca.dhoelzeman.console.Event;
 
 import javax.swing.*;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class EventPanel extends JPanel {
     private final Event event;
     private JButton completeButton;
 
-    public EventPanel(Event e) {
+    public EventPanel(Event e, EventListPanel eventListPanel) {
         event = e;
+
+        ArrayList<String> displayStrings = event.getDisplayStrings();
+
+        for (String displayString : displayStrings) {
+            add(new JLabel(displayString));
+        }
+
 
         if (event instanceof Completable) {
             completeButton = new JButton("Mark Complete");
 
             add(completeButton);
+
+            completeButton.addActionListener(_ -> {
+                ((Completable) event).complete();
+                this.remove(completeButton);
+                completeButton.setVisible(false);
+                eventListPanel.addOrUpdateEvents();
+            });
         }
-
-    }
-
-    // Set background color according to the urgency
-    public void updateUrgency() {
 
     }
 }
